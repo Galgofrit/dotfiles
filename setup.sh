c7aeeb660abd20c8c8a68e877839c506d9228f0b
Gal Gofrit <galg@sentinelone.com>
2019-04-06 15:27:30 +0300

    Adding basic package manager detector to setup.sh, for testing

--- setup.sh
+++ setup.sh
@@ -1,13 +1,43 @@
 #!/bin/sh
 
+OS=$(uname)
+
+if [ "$OS" = "Darwin" ]; then
+    PKG_MANAGER="brew install"
+elif [ "$OS" = "Linux" ]; then
+    if [ $(lsb_release -is) = "Raspbian" ] || \
+        [ $(lsb_release -is) = "Debian" ] || \
+        [ $(lsb_release -is) = "Ubuntu" ]; then
+        PKG_MANAGER="apt get install "
+    fi
+else
+    echo "Unsupported OS."
+    exit 1
+fi
+
+
+echo $PKG_MANAGER
+exit 0
+
 # install git
 # install tmux
 # instal vim
+# install stow
+
+# install ag the silver searcher
+# install ctags
+# install fzf # ! NOT  REQUIRED, INSTALLED VIA VIM PLUG
 
 # install flake8
 # install pylint
 # install yapf
 # install autopep8
+# jedi vim support
+
+#pip3 install jedi
+
+#brew install codequery
+#sudo pip install pyscope
 
 
 stow bash
