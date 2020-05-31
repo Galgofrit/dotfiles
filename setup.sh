956e0434985d55df269e91a375bc5a743e9f9275
Gal Gofrit <galg@sentinelone.com>
2020-05-31 13:34:43 +0300

    Installing Zsh via install.sh, install YouCompleteMe with clang completer

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ---
+# Utilities
+
 OS=$(uname)
 
 if [ "$OS" = "Darwin" ]; then
@@ -21,13 +24,21 @@ function install ()
     eval "$PKG_MANAGER $1"
 }
 
+
 # ---
 # Setup
+echo "Installing Brew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
 echo "Setting up environment..."
 install git
 install tmux
 install vim
 install stow
+
+# Stow Zsh before installing it to avoid conflict
+stow zsh
+install zsh
 
 if [ "$OS" = "Darwin" ]; then
     install the_silver_searcher
@@ -35,25 +46,35 @@ elif [ "$OS" = "Linux" ]; then
     install silversearcher-ag
 fi
 install ctags
-# install fzf # not required, installed via vimplug
 
 # Language style/linting
 install flake8
 install pylint
 install yapf
 install autopep8
+install cmake
 jedi vim support
 
-#pip3 install jedi
+#Install OhMyZsh
+echo "Installing oh my zsh..."
+curl -Lo install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
+rm install.sh
+
+#pip3 install jedi TODO
 
 stow bash
 stow git
 stow tmux
 stow vim
-stow zsh
 
 vim -E -c PlugUpdate -c q # Update vim-plugged
 vim -E -c PlugInstall -c q # Install all Vim plugins
+
+# Install YouCompleteMe
+echo "Installing YCM..."
+~/.vim/plugged/YouCompleteMe/third_party/ycmd/build.py --clang-completer
+
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh # Install Tmux plugins
 
 # Set up fonts
